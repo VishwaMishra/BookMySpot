108777415e8a001b8c8afc39262e997856107842
VishwaMishra <vishwabeit20@gmail.com>
2021-03-14 13:35:48 +0530

    "Admin Login Screen Created."

---
+++ app/src/main/java/com/example/meetingroombookingsystem/AdminDashboard.java
@@ -0,0 +1,29 @@
+package com.example.meetingroombookingsystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminDashboard extends AppCompatActivity {
+    ImageButton imgBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_dashboard);
+        ImageButton imgBtn = findViewById(R.id.backButton);
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+//    public void back_home(){
+//
+//    }
+}
---
+++ app/src/main/java/com/example/meetingroombookingsystem/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.meetingroombookingsystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText user, pass;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText user = findViewById(R.id.user);
+        final EditText pass = findViewById(R.id.pass);
+        Button login = (Button) findViewById(R.id.loginBtn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+                final String name = user.getText().toString();
+                final String passW = pass.getText().toString();
+
+                if (true){
+                        Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+
+                        Intent i = new Intent(getApplicationContext(),AdminDashboard.class);
+                        startActivity(i);
+
+                    }
+//             
+            }
+
+        });
+
+
+    }
+}
